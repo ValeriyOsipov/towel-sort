cb64cba2b07bb5dbf3e05d56795d90405bb5dcb7
Valeriy Osipov <freebs@mail.ru>
2020-10-07 13:12:43 +0400

    fix: fixed validation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 module.exports = function towelSort (matrix) {
+  if (matrix === undefined) {
+    return [];
+  }
   for (let i = 0; i < matrix.length; i++) {
     if (i % 2 === 1) {
       matrix[i].sort(function(a, b){return b - a});
